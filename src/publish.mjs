9cd72f14d0f9002f12b2fcecb42fdfd9a1635877
KARM PATEL <96860884+Karmpatel960@users.noreply.github.com>
2024-02-03 21:05:15 +0530

    GITSHA ADDED

--- src/publish.mjs
+++ src/publish.mjs
@@ -68,7 +68,7 @@ const getGitProjectName = async () => {
 
 const getGitCommitDetails = async () => {
   try {
-    const commitSha = process.env.GITHUB_SHA;
+    const commitSha = getSecret('GITHUB_SHA');
     const apiUrl = `https://api.github.com/repos/${getSecret('REPO_TOKEN')}/commits/${commitSha}`;
     
     const response = await axios.get(apiUrl, {
@@ -175,7 +175,7 @@ const getGitCommitDetails = async () => {
 
 const getGitDiff = async () => {
   const repoToken = getSecret("REPO_TOKEN");
-  const commitHash = process.env.GITHUB_SHA;
+  const commitHash = getSecret('GITHUB_SHA');
 
   try {
     const repoInfoUrl = `https://api.github.com/repos/${getSecret('REPO_TOKEN')}`;
